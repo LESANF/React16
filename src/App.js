0822a67669e752cc16f5f8e09b93f64a800b0b0d
LESANF <nagong1000@naver.com>
2020-02-10 04:41:56 +0900

    docs React 16 ING

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RealPortal extends Component {
+  render() {
+    return createPortal(<A />, document.getElementById("king"));
+  }
+}
+
+const A = () => "This is Portal";
+
+class Potato extends Component {
+  render() {
+    return "SEX111";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Potato />
+        <RealPortal />
+      </>
+    );
+  }
 }
 
 export default App;
